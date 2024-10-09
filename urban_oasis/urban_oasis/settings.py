3cf76f6a686a13b313f3487c8f583c1b78639796
EdwardMase <105016548+Doneddie@users.noreply.github.com>
2024-10-09 17:41:19 +0000

    New changes to database

--- urban_oasis/urban_oasis/settings.py
+++ urban_oasis/urban_oasis/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'urban_oasis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'urban_oasis',
-        'USER': 'urban_oasis_user',
-        'PASSWORD': 'Doneddie@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
